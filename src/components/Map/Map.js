95f8b3ed789fe99da9936ffd3668b8e2dd4d2a16
Abdul Kader <abdulkadirshohan@gmail.com>
2021-09-21 20:35:50 +0600

    update

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,21 +6,26 @@ import {Rating} from "@material-ui/lab";
 
 import useStyles from './styles'
 
-const Map =()=>{
+const Map =({setCoordinates,setBounds,coordinates})=>{
     const classes=useStyles()
     const isMobile=useMediaQuery('(min-width:600px)')
 
-    const coordinates={lat:0,lng:0}
     return(
         <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{keys:'AIzaSyA7wy3oACS4pB02ZPXtenyn7d-jKNbCxys'}}
+                bootstrapURLKeys={{ key: 'AIzaSyBQWeQ8GO9ZXdEC3SR5GEyQJULO7nZ28_Q' }}
                 defaultCenter={coordinates}
                 center={coordinates}
-                margin={[50,50,50,50]}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
                 options={''}
-                onChange={''}
-                onChildClick={''}
+                onChange={(e) => {
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+
+
+                }}
+
             >
 
 
--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -1,8 +1,8 @@
 import React from "react";
-const PaceDetails =()=>{
+const PaceDetails =({place})=>{
     return(
         <>
-
+            <h1>{place.name}</h1>
         </>
 
     );
